9f80c4881da580c00c7de95d79fccc1677b397ba
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-04-28 20:57:20 -0400

    minor changes to script file to start calculating current time for refference.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@
 // get today's date in appropriate format and append to timeblock.
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
+
+// variable to retrieve current hour
+var currentHour = moment().format("H");
+console.log(currentHour);
+
+var plans = [];
+var plans = JSON.parse(window.localStorage.getItem("planData")) || []
+
+//make time blocks
