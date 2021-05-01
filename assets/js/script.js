40d36e337490ad2e75315024e174d42f98bb8972
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-04-30 22:14:40 -0400

    added jquery and javascript coding to handle sending data to local storage and pulling it back when needed to populate textarea in time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,56 +9,58 @@ $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 var currentHour = moment().format("H");
 console.log(currentHour);
 
-//make time blocks
-// confirm if appointment data exists in local storage or not and populate time blocks.
-if ( localStorage.getItem("planData") === null ) {
-    console.log("no data ian")
-    for ( var i=9; i < 18; i++){
-        console.log(i);
-        var timeBlock = $('<div>');
-        timeBlock.addClass("row time-block");
-            if ( i < currentHour ) {
-                timeBlock.addClass('past');
-            } else if ( i == currentHour ) {
-                timeBlock.addClass('present');
-            } else {
-                timeBlock.addClass('future')
-            }
-        var hourLabel = $('<div>');
-        hourLabel.addClass('hour col-1');
-        if (i < 12 ) {
-            hourLabel.text(i + "AM")
-        } else if (i == 12) {
-            hourLabel.text(i + "PM")
+//code to make time blocks and append to the container contained in the HTML.
+for ( var i=9; i < 18; i++){
+    console.log(i);
+    var timeBlock = $('<div>');
+    // adds color formating for the rows based on where they are relative to current time.
+    timeBlock.addClass("row time-block");
+        if ( i < currentHour ) {
+            timeBlock.addClass('past');
+        } else if ( i == currentHour ) {
+            timeBlock.addClass('present');
         } else {
-            // code to make hours display as 12hr clock instead of 24 hr clock
-            var regTime = i;
-            regTime -= 12;
-            hourLabel.text(regTime + "PM")
+            timeBlock.addClass('future')
+        }
+    var hourLabel = $('<div>');
+    hourLabel.addClass('hour col-1');
+    if (i < 12 ) {
+        hourLabel.text(i + "AM")
+    } else if (i == 12) {
+        hourLabel.text(i + "PM")
+    } else {
+        // code to make hours display as 12hr clock instead of 24 hr clock
+        var regTime = i;
+        regTime -= 12;
+        hourLabel.text(regTime + "PM")
+    };
+    timeBlock.append(hourLabel);
+    var activityBlank = $('<textarea>').addClass('event col-10');
+        // checks to see if there is saved datat to populate in the time blocks.
+        if (localStorage.getItem("plansData") === null ) {
+            activityBlank.text('');
+        } else {
+            var hourMatch = i;
+            var oldEvent = JSON.parse(window.localStorage.getItem('plansData'));
+            hourMatch -= 9;
+            activityBlank.text(oldEvent[hourMatch]);
         };
-        timeBlock.append(hourLabel);
-        var activityBlank = $('<textarea>').addClass('event col-10');
-        timeBlock.append(activityBlank);
-        var saveButton = $('<button>').addClass('saveBtn col-1 fas fa-save');
-        timeBlock.append(saveButton);
-        eventList.append(timeBlock);
-    }
-};
+    timeBlock.append(activityBlank);
+    var saveButton = $('<button>').addClass('saveBtn col-1 fas fa-save');
+    timeBlock.append(saveButton);
+    eventList.append(timeBlock);
+}
 
 
-// put event data in local storage when clicking save button.
+// variable to hold event data when entered in main page as an array.
 var plans = [];
 // put event data in local storage when clicking save button.
 $('.saveBtn').on('click', function() {
-    console.log("save");
     $('.event').each(function(){
-        console.log(this.value)
         plans.push(this.value);
     });
-    console.log(plans)
+    localStorage.setItem("plansData", JSON.stringify(plans));
 } )
 
-
-// var plans = JSON.parse(window.localStorage.getItem("planData")) || []
 
 
