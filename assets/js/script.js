c09dd726db92a9abbc5d5d3df80f4ec26f7c2dd6
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-04-30 21:33:51 -0400

    added jquery tools to handle creating an array from the events entered to place in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// variables for DOM elements.
+// variables for DOM elements to append time blocks.
 var eventList = $('.container');
 
 // get today's date in appropriate format and append to timeblock.
@@ -8,9 +8,6 @@ $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 // variable to retrieve current hour
 var currentHour = moment().format("H");
 console.log(currentHour);
-
-
-var plans = [];
 
 //make time blocks
 // confirm if appointment data exists in local storage or not and populate time blocks.
@@ -34,19 +31,34 @@ if ( localStorage.getItem("planData") === null ) {
         } else if (i == 12) {
             hourLabel.text(i + "PM")
         } else {
+            // code to make hours display as 12hr clock instead of 24 hr clock
             var regTime = i;
             regTime -= 12;
             hourLabel.text(regTime + "PM")
         };
         timeBlock.append(hourLabel);
-        var activityBlank = $('<textarea>').addClass('col-10');
+        var activityBlank = $('<textarea>').addClass('event col-10');
         timeBlock.append(activityBlank);
         var saveButton = $('<button>').addClass('saveBtn col-1 fas fa-save');
         timeBlock.append(saveButton);
         eventList.append(timeBlock);
     }
- 
-}
+};
+
+
+// put event data in local storage when clicking save button.
+var plans = [];
+// put event data in local storage when clicking save button.
+$('.saveBtn').on('click', function() {
+    console.log("save");
+    $('.event').each(function(){
+        console.log(this.value)
+        plans.push(this.value);
+    });
+    console.log(plans)
+} )
+
+
 // var plans = JSON.parse(window.localStorage.getItem("planData")) || []
 
 
